76b424bf485c0e6f2f4e09049c81396fad8f91e2
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-02-03 23:16:55 +0800

    Customised login url

---
+++ a2-s3673712-s3719368/a2-s3673712-s3719368/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using a2_s3673712_s3719368.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace a2_s3673712_s3719368.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly NationBankContext _context;
+        public AdminController(NationBankContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
