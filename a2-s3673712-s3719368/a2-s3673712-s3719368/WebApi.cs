3fc07031c272559fc8b7dbad06aebb769ca7b1f7
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-02-04 18:14:40 +0800

    web api

---
+++ a2-s3673712-s3719368/a2-s3673712-s3719368/WebApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace a2_s3673712_s3719368
+{
+    public class WebApi
+    {
+        private const string ApiBaseUri = "http://localhost:59505";
+        public static HttpClient InitializeClient()
+        {
+            var client = new HttpClient { BaseAddress = new Uri(ApiBaseUri) };
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
