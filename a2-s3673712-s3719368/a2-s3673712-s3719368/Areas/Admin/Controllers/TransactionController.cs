997e95bd0db1b5b3ede81a4c1620ca554b98f4cb
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-02-05 00:10:23 +0800

    Create transaction page

---
+++ a2-s3673712-s3719368/a2-s3673712-s3719368/Areas/Admin/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using a2_s3673712_s3719368.Area.Admin.Models;
+using a2_s3673712_s3719368.Areas.Admin.Controllers.Managers;
+using a2_s3673712_s3719368.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace a2_s3673712_s3719368.Areas.Admin.Controllers
+{
+    [AuthorizeAdmin]
+    public class TransactionController : Controller
+    {
+        private AccountManger accountManger;
+        public TransactionController() 
+        {
+            accountManger = new AccountManger();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SelectTransaction() 
+        {
+            IEnumerable<AccountDto> accounts = await accountManger.GetAllAccounts();
+            return View(accounts);
+        }
+    }
+}
