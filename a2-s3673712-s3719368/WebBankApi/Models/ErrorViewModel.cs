6f10c0672d1da29ca8624407f869c3b38d34516a
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-02-04 14:05:06 +0800

    Init

---
+++ a2-s3673712-s3719368/WebBankApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace a2_s3673712_s3719368.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
